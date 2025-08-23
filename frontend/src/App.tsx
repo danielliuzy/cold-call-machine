5eec5c95f3bcf2f489f2039a23cbdc6c15df1c9a
Daniel Liu <danielliuzy@fb.com>
2025-08-22 23:05:36 -0700

    add shadcn

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,12 @@
+import { Button } from "./components/ui/button";
+
 function App() {
-  return <h1>Hello world</h1>;
+  return (
+    <>
+      <h1>Hello world</h1>
+      <Button>Hello</Button>
+    </>
+  );
 }
 
 export default App;
