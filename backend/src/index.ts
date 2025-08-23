242cb1953b7b165febbab544c01a9913fcbfe390
Daniel Liu <danielliuzy@fb.com>
2025-08-22 19:49:07 -0700

    empty

---
+++ backend/src/index.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+const PORT = process.env["PORT"] || 3001;
+
+app.use(cors());
+
+app.get("/", (_, res) => {
+  res.json({ message: "online!" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
