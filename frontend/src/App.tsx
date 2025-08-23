b43a6a0677f6c9059574cf82dbc14fcdb81bc4c1
Daniel Liu <danielliuzy@fb.com>
2025-08-23 10:50:00 -0700

    Temporary Commit at 8/23/2025, 10:50:00 AM

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
+import { useState } from "react";
 import { Button } from "./components/ui/button";
+import { Input } from "./components/ui/input";
 
 function App() {
+  const [url, setUrl] = useState("");
   return (
-    <>
-      <h1>Hello world</h1>
-      <Button>Hello</Button>
-    </>
+    <div className="h-screen w-screen px-1/2 bg-red-300">
+      <Input value={url} onChange={(e) => setUrl(e.target.value)} />
+    </div>
   );
 }
 
